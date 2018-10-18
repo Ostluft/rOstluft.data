07babea188c7bbc1b1523088b5ff6357f5041c1e
Thomas von Allmen <42243088+ugzvot@users.noreply.github.com>
2018-10-18 15:13:20 +0200

    Initial Commit

---
+++ R/rOstluft.data.R
@@ -0,0 +1,20 @@
+#' rOstluft.data: Airquality data from zuerich for 2010-2014
+#'
+#' This Package contains air quality data from the measurement sites
+#' Zuerich Stampfenbachstrasse, Zuerich Schimmelstrasse and Zuerich
+#' Rosengartenstrasse for the years 2010 until 2014. The time resolution
+#' is 30 minutes.
+#' The goal for rOstluft.data is to provide data in a raw format for examples
+#' and test of the package rOstluft.
+#'
+#' @docType package
+#' @name rOstluft.data
+#'
+#' @examples
+#'  path_to_file <- system.file("extdata", "Zch_Stampfenbachstrasse_2010-2014.csv",
+#'     package = "rOstluft.data", mustWork = TRUE)
+#'  print(path_to_file)
+#'
+#'  list.files(system.file("extdata", package = "rOstluft.data"))
+#'
+NULL
